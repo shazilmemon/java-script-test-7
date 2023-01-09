375326ed9dd1501d60b3d98eea2d74f2b7b910ca
Shazil <shazilmemon6@gmail.com>
2023-01-09 17:26:20 +0500

    javascript

---
+++ app.js
@@ -0,0 +1,73 @@
+// chapters 35 to 38
+
+// answer no 1
+
+// var clickme = function() {
+//     var c = new Date();
+//      document.getElementById("showdate").innerHTML=c;
+// }
+// answer no 2
+
+// function name1(name1) {
+//     alert("shazil")
+// }
+
+// function name2(name2) {
+//     alert("ahmed")
+// }
+
+// function name3(name3) {
+//     alert("shail ahmed")
+// }
+
+// name1();
+// name2();
+// name3();
+
+// answer no 3
+
+//  function myName() {
+//     var a = prompt("Enter first number");
+//     var b = prompt("Enter second number");
+
+//     alert(a + b);
+
+// };
+// myName();
+
+// ansswer no 4
+// var para = document.getElementById("para");
+// function calc(num1 = 10, opr = "+", num2 = 9) {
+//   if (opr === "+") {
+//     return num1 + num2;
+//   } else if (opr === "-") {
+//     return num1 - num2;
+//   } else if (opr === "*") {
+//     return num1 * num2;
+//   } else {
+//     return "incorrect opera!";
+//   }
+// }
+// para.innerHTML = calc();
+
+// answer no 5
+// function myFunction(a) {
+    
+//     console.log(a**2)
+
+// }
+// var p = prompt("val")
+// myFunction(p)
+
+
+// answer no 6  not understand
+
+// answer no 7
+
+function counting() {
+var a = prompt("enter your number")
+}
+
+
+
+
